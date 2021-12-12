bfd992fa41c11f0926adf9325cfc1e21a8d1e21d
sbsunil251 <sbsunil251@gmail.com>
2021-12-12 22:53:47 +0530

    sdsdf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,17 @@
-provider "azurerm" {
-  version = "=2.0.0"
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+provider "azurerm" {  
   features {}
+  subscription_id   = "31b625db-7920-423e-88d0-df5b6419e701"
+  tenant_id         = "a976c852-d327-4927-be05-6650129d208c"
+  client_id         = "e5c0266f-4f9a-44b6-bd5f-c203447ef992"
+  client_secret     = "<service_principal_password>"
 }
 
 terraform {
